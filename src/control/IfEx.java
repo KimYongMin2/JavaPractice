0ca09fd83727b299a4b2c2d87b6d1a668651072b
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-04-06 15:43:52 +0900

    if조건문 확인

---
+++ src/control/IfEx.java
@@ -0,0 +1,14 @@
+package control;
+
+public class IfEx {
+    public static void main(String[] args) {
+        int age = 2;
+
+        if (age >= 8) {
+            System.out.println("학교에 다닙니다.");
+        }
+        else {
+            System.out.println("학교에 다니지 않습니다");
+        }
+    }
+}
