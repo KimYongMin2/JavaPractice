e812037bdcbf0fdd60e2482246a1a88cd8f6fc0c
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-04-06 20:08:44 +0900

    간략한 class함수

---
+++ src/class_Object/Student.java
@@ -0,0 +1,24 @@
+package class_Object;
+
+public class Student {
+
+    int studentId;
+    String studentName;
+    int grade;
+    String address;
+
+    public void showStudentInfo(){
+        System.out.println("학번 : " + studentId + "\n학년 : " + grade + "\n이름 : " +studentName);
+        System.out.println("사는 곳 : " + address);
+    }
+
+    public static void main(String[] args) {
+        Student student_01 = new Student();
+        student_01.studentId = 100101;
+        student_01.studentName = "김용민";
+        student_01.grade = 3;
+        student_01.address = "서울";
+        student_01.showStudentInfo();
+    }
+}
+
