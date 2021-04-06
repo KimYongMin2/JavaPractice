66a2791075d5bc0c443102d5225778cb1229a10b
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-04-06 10:51:10 +0900

    최댓값과 최솟값 표현

---
+++ src/second/VariableEx.java
@@ -0,0 +1,13 @@
+package second;
+
+public class VariableEx {
+    public static void main(String[] args) {
+
+        byte bData = -128;
+        System.out.println(bData);
+
+        byte bData2 = 127;
+        System.out.println(bData2);
+
+    }
+}
