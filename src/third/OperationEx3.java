8eb0845578160cdec8cfcc0f4d1cb547c6a43f86
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-04-06 15:23:13 +0900

    복합대입연산자

---
+++ src/third/OperationEx3.java
@@ -0,0 +1,14 @@
+package third;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        System.out.println(num1 += 1);
+
+        System.out.println(num1 %= 10);
+        num1 -=1;
+        System.out.println(num1);
+            
+        }
+    }
+}
