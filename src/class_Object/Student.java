c8b2d37882470cc1640c78d8526bcdd6744e89e6
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-04-07 10:29:42 +0900

    메인문 따로 관리

--- src/class_Object/Student.java
+++ src/class_Object/Student.java
@@ -20,13 +20,6 @@ public class Student {
         studentName = name;
     }
 
-    public static void main(String[] args) {
-        Student student_01 = new Student();
-        student_01.studentId = 100101;
-        student_01.studentName = "김용민";
-        student_01.grade = 3;
-        student_01.address = "서울";
-        student_01.showStudentInfo();
-    }
+
 }
 
