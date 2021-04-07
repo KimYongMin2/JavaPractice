e7b28b56cf0f23d74a6d4e04de7772240d6a28ce
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-04-07 11:24:32 +0900

    뚜닥뚜닥 분리

--- src/practice/Student.java
+++ src/practice/Student.java
@@ -5,39 +5,35 @@ import java.util.Scanner;
 public class Student {
     int studentID;
     int grade;
+    int birth;
     String studentName;
     String address;
     String dep;
 
-    public void setStudentName() {
-        System.out.println("이름을 입력해주세요");
-        Scanner sc = new Scanner(System.in);
-        studentName = String.valueOf(sc.nextLine());
+    public void setStudentName(String studentName){
+        this.studentName = studentName;
+    }
+
+    public void setGrade(int grade){
+        this.grade = grade;
+    }
+
+    public void setBirth(int birth) {
+        this.birth =birth;
     }
 
     public void setStudentID() {
-        System.out.println("학번을 입력해주세요");
-        Scanner sc = new Scanner(System.in);
-        studentID = Integer.valueOf(sc.nextLine());
+        studentID = birth+21000000;
     }
 
-    public void setGrade() {
-        System.out.println("학년을 입력해주세요");
-        Scanner sc = new Scanner(System.in);
-        grade = Integer.valueOf(sc.nextLine());
+    public void setAddress(String address){
+        this.address = address;
     }
 
-    public void setAddress() {
-        System.out.println("주소를 입력해주세요");
-        Scanner sc = new Scanner(System.in);
-        address = String.valueOf(sc.nextLine());
+    public void setDep(String dep){
+        this.dep = dep;
     }
 
-    public void setDep() {
-        System.out.println("학과를 입력해주세요");
-        Scanner sc = new Scanner(System.in);
-        dep = String.valueOf(sc.nextLine());
-    }
 
     public void showStudentInfo(){
         System.out.println("학과 : " + dep + "\n학번 : " + studentID);
@@ -45,14 +41,4 @@ public class Student {
         System.out.println("사는 곳 : " + address);
     }
 
-    public static void main(String[] args) {
-        Student student_01 = new Student();
-        student_01.setDep();
-        student_01.setStudentID();
-        student_01.setGrade();
-        student_01.setStudentName();
-        student_01.setAddress();
-
-        student_01.showStudentInfo();
-    }
 }
