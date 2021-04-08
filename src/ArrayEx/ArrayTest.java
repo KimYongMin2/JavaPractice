24f10ce579c680568caafb8c23d3ff2e21c86914
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-04-08 21:33:15 +0900

    배열

---
+++ src/ArrayEx/ArrayTest.java
@@ -0,0 +1,15 @@
+package ArrayEx;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int numbers[] = new int[] {0 , 1 ,2};
+        System.out.println(numbers.length);
+        int test[] = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            test[i] = 0;
+        }
+
+
+
+    }
+}
